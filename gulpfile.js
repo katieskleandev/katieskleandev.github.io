5fb80e75f573fa549cc9acfe70ce3458772fd4a0
katiesklean <katieskleandev@gmail.com>
2018-08-09 20:04:08 +0100

    added privacy policy and refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ let gulp = require('gulp')
 let child = require('child_process')
 let htmlmin = require('gulp-htmlmin')
 let cleanCSS = require('gulp-clean-css')
-let uglify = require('gulp-uglifyjs')
+let minify = require('gulp-minify')
 let ghPages = require('gulp-gh-pages')
 
-gulp.task('default', ['jekyll-serve'])
 
 gulp.task('jekyll-serve', (gulpCallBack) => {
   let jekyll = child.spawn('jekyll', ['serve', '--config', 'jekyll/_config.yml'])
@@ -17,6 +16,28 @@ gulp.task('jekyll-serve', (gulpCallBack) => {
   })
 })
 
+gulp.task('default', gulp.series('jekyll-serve'))
+
+gulp.task('minify-html', () => {
+  return gulp.src('dist/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true, removeComments: true, minifyJS: true, minifyCSS: true}))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-css', () => {
+  return gulp.src('dist/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-js', () => {
+  return gulp.src('dist/**/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify', gulp.series('minify-html', 'minify-css', 'minify-js'))
+
 gulp.task('jekyll-build', (gulpCallBack) => {
   let jekyll = child.spawn('jekyll', ['build', '--config', 'jekyll/_config.yml,jekyll/_production_config.yml'])
 
@@ -27,29 +48,11 @@ gulp.task('jekyll-build', (gulpCallBack) => {
   })
 })
 
-gulp.task('minify', ['minify-html', 'minify-css', 'minify-js'])
+gulp.task('build', gulp.series('jekyll-build', 'minify'))
 
-gulp.task('minify-html', ['jekyll-build'], () => {
-  return gulp.src('dist/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true, minifyJS: true, minifyCSS: true}))
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('minify-css', ['jekyll-build'], () => {
-  return gulp.src('dist/**/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('minify-js', ['jekyll-build'], () => {
-  return gulp.src('dist/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('build', ['minify'])
-
-gulp.task('deploy', ['build'], () => {
+gulp.task('deploy-to-github', () => {
   return gulp.src('dist/**/*')
     .pipe(ghPages({branch: 'master'}))
 })
+
+gulp.task('deploy', gulp.series('build', 'deploy-to-github'))
